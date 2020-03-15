2c5c6a65711f1af9dc928fd8c57795c676bae3cc
selifu <affu1412@gmail.com>
2020-03-15 11:18:00 +0800

    補增 carousel indexbook 的key

--- model/main_carouselAll.js
+++ model/main_carouselAll.js
@@ -1,40 +1,34 @@
 
 
-    let arrLists_a = ['images/newbook_img/banner4-3.jpg',
-                    "綠野先蹤2",
-                    "自從艾薇·羅伯茲的女兒在父親那得到了一個玩偶後，感覺就全變個樣了\n因此特地請來了名人來解決此事，怎料這事情才正要開始……"
-                    ];
-    let arrLists_b = ['images/newbook_img/banner1-3.jpg',
-                    "解不開的神秘血案",
-                    "來自羅伯茲女士的委託，我與偵探老友來到了白銀莊園\n以女僕上吊開始發生的一連串詭異血案……\n吾友，這次奇怪的地方太多了，總覺得有人在窺看我們"
-                    ];
-    let arrLists_c = ['images/newbook_img/banner2-3.jpg',
-                    "360小時指考衝",
-                    "內附─學長的躲過監考老師72招祕笈\n重考10年生親身經驗"
-                    ];
-    let arrLists_d = ['images/newbook_img/banner3-3.jpg',
-                    "中國古文108精選（一）",
-                    "集結假古文108篇，每篇30小節，每小節都有專人附註詳解\n共三千頁的豪華大集合"
-                    ];
-
-  function CarouselItemActive(props){
-    return(
-      <div className="carousel-item active">
-        <img className="d-block w-100" src={props.newbookimg} alt="First slide" />
-        <div className="carousel-caption d-none d-md-block">
-          <div className="newbook_div">
-            <h5 className="newbook_title">{props.title}</h5>
-            <p className="newbook_content new-line">{props.content}</p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    let car_Lists_a = {id:'a',
+                      clasact:"carousel-item active",
+                      img:'images/newbook_img/banner4-3.jpg',
+                      title:"綠野先蹤2",
+                      content:"自從艾薇·羅伯茲的女兒在父親那得到了一個玩偶後，感覺就全變個樣了\n因此特地請來了名人來解決此事，怎料這事情才正要開始……"
+                    };
+    let car_Lists_b = {id:'b',
+                      clasact:"carousel-item",
+                      img:'images/newbook_img/banner1-3.jpg',
+                      title:"解不開的神秘血案",
+                      content:"來自羅伯茲女士的委託，我與偵探老友來到了白銀莊園\n以女僕上吊開始發生的一連串詭異血案……\n吾友，這次奇怪的地方太多了，總覺得有人在窺看我們"
+                    };
+    let car_Lists_c = {id:'c',
+                      clasact:"carousel-item",
+                      img:'images/newbook_img/banner2-3.jpg',
+                      title:"360小時指考衝",
+                      content:"內附─學長的躲過監考老師72招祕笈\n重考10年生親身經驗"
+                    };
+    let car_Lists_d = {id:'d',
+                      clasact:"carousel-item",
+                      img:'images/newbook_img/banner3-3.jpg',
+                      title:"中國古文108精選（一）",
+                      content:"集結假古文108篇，每篇30小節，每小節都有專人附註詳解\n共三千頁的豪華大集合"
+                    };
 
   function CarouselItem(props){
     return(
-      <div className="carousel-item">
-        <img className="d-block w-100" src={props.newbookimg} alt={props.noimg} />
+      <div className={props.clasact}>
+        <img className="d-block w-100" src={props.img} alt="CarouselImg" />
         <div className="carousel-caption d-none d-md-block">
           <div className="newbook_div">
             <h5 className="newbook_title">{props.title}</h5>
@@ -47,26 +41,20 @@
 
   class CarouselAll extends React.Component{
     render(){
-      let arrLists = [arrLists_a,arrLists_b,arrLists_c,arrLists_d];
+      let car_Lists = [car_Lists_a,car_Lists_b,car_Lists_c,car_Lists_d];
 
-      let lists = [];
-
-      for(let i=0;i<=arrLists.length-1;i++){
-        if(i==0){
-          lists.push(<CarouselItemActive key={arrLists[i]}
-            newbookimg={arrLists[i][0]} 
-            title={arrLists[i][1]} 
-            content={arrLists[i][2]} />);
-        }else{
-          lists.push(<CarouselItem key={arrLists[i].toString()}
-            newbookimg={arrLists[i][0]} 
-            noimg="newIMG" title={arrLists[i][1]} 
-            content={arrLists[i][2]} />);
-        }
-      }
+      let carLists = car_Lists.map(function(lists){
+        return(
+          <CarouselItem key={lists.id}
+            clasact={lists.clasact}
+            img={lists.img}
+            title={lists.title}
+            content={lists.content} />
+        )
+      })
       return (
         <div>
-          {lists}
+          {carLists}
         </div>
       )
     }
--- model/main_hotbookAll.js
+++ model/main_hotbookAll.js
@@ -1,40 +1,40 @@
-  let  ob_list_a = ["bookimg/001.jpg",
-                    "沉默的遊行",
-                    "東野圭吾",
-                    "420",
-                    "?do=product_book&ob_id=1"
-                  ];
-  let  ob_list_b = ["bookimg/002.jpg",
-                    "天藍色的謀殺案",
-                    "莎拉．J．哈里斯",
-                    "420",
-                    "?do=product_book&ob_id=2"
-                  ];
-  let  ob_list_c = ["bookimg/003.jpg",
-                    "孤宿之人（上下冊．經典回歸版）",
-                    "宮部美幸	",
-                    "599",
-                    "?do=product_book&ob_id=3"
-                  ];
+  let  ob_list_a = {id:"?do=product_book&ob_id=1",
+                    img:"bookimg/001.jpg",
+                    name:"沉默的遊行",
+                    author:"東野圭吾",
+                    price:"420"
+                  };
+  let  ob_list_b = {id:"?do=product_book&ob_id=2",
+                    img:"bookimg/002.jpg",
+                    name:"天藍色的謀殺案",
+                    author:"莎拉．J．哈里斯",
+                    price:"420"
+                  };
+  let  ob_list_c = {id:"?do=product_book&ob_id=3",
+                    img:"bookimg/003.jpg",
+                    name:"孤宿之人（上下冊．經典回歸版）",
+                    author:"宮部美幸	",
+                    price:"599"
+                  };
 
-  let tr_list_a = ["bookimg/008-1.jpg",
-                  "大人絕景旅 京都：世界遺產×京料理×京雜貨，探尋  古都歲時風物詩",
-                  "朝日新聞出版",
-                  "360",
-                  "?do=product_book&ob_id=6"
-                ];
-  let tr_list_b = ["bookimg/009.jpg",
-                  "歐洲從東邊玩：波羅的海三小國、德東、波蘭、捷奧匈金三角，安全×超值×好吃×好玩，小資族也可以輕鬆上路",
-                  "背包Ken（吳宜謙）",
-                  "380",
-                  "?do=product_book&ob_id=7"
-                ];
-  let tr_list_c = ["bookimg/no_book_img.jpg",
-                  "辣椒的世界史：橫跨歐亞非的尋味旅程，一場熱辣過癮的餐桌革命",
-                  "王恬中",
-                  "380",
-                  "?do=product_book&ob_id=9"
-                ];
+  let tr_list_a = {id:"?do=product_book&ob_id=6",
+                  img:"bookimg/008-1.jpg",
+                  name:"大人絕景旅 京都：世界遺產×京料理×京雜貨，探尋  古都歲時風物詩",
+                  author:"朝日新聞出版",
+                  price:"360"
+                };
+  let tr_list_b = {id:"?do=product_book&ob_id=7",
+                  img:"bookimg/009.jpg",
+                  name:"歐洲從東邊玩：波羅的海三小國、德東、波蘭、捷奧匈金三角，安全×超值×好吃×好玩，小資族也可以輕鬆上路",
+                  author:"背包Ken（吳宜謙）",
+                  price:"380"
+                };
+  let tr_list_c = {id:"?do=product_book&ob_id=9",
+                  img:"bookimg/no_book_img.jpg",
+                  name:"辣椒的世界史：橫跨歐亞非的尋味旅程，一場熱辣過癮的餐桌革命",
+                  author:"王恬中",
+                  price:"380"
+                };
 
 
   function HotbookInface(props){
@@ -63,19 +63,18 @@
     render(){
       let ob_list = [ob_list_a,ob_list_b,ob_list_c];
 
-      let oblist = [];
-
-      for(let i=0;i<=ob_list.length-1;i++){
-        oblist.push(
-          <div className="col-md-4">
-            <HotbookInface key = {ob_list[i]} 
-              img={ob_list[i][0]}
-              name={ob_list[i][1]}
-              author={ob_list[i][2]}
-              price={ob_list[i][3]}
-              id={ob_list[i][4]} />
-          </div>);
-      }
+      let oblist = ob_list.map(function(list){
+        return(
+          <div className="col-md-4" key={list.id}>
+            <HotbookInface
+              id={list.id}
+              img={list.img}
+              name={list.name}
+              author={list.author}
+              price={list.price} />
+          </div>
+        )
+      })
 
       return(
         <div className="row">
@@ -88,19 +87,18 @@
   class TravelbookAll extends React.Component{
     render(){
       let tr_list=[tr_list_a,tr_list_b,tr_list_c];
-      let trlist = [];
-
-      for(let i=0;i<=tr_list.length-1;i++){
-        trlist.push(
-          <div className="col-md-4">
-            <HotbookInface key={tr_list[i]}
-              img={tr_list[i][0]}
-              name={tr_list[i][1]}
-              author={tr_list[i][2]}
-              price={tr_list[i][3]}
-              id={tr_list[i][4]} />
-          </div>);
-      }
+      let trlist = tr_list.map(function(list){
+        return(
+          <div className="col-md-4" key={list.id}>
+            <HotbookInface
+              id={list.id}
+              img={list.img}
+              name={list.name}
+              author={list.author}
+              price={list.price} />
+          </div>
+        )
+      })
       return(
         <div className="row">
           {trlist}
